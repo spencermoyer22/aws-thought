f85d084b8516dee81e1b8a1c322af594639abfa5
Big Head <spencer.moyer16@yahoo.com>
2021-08-31 15:09:12 -0700

    created route to add thought for a user

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -49,4 +49,24 @@ router.get('/users/:username', (req, res) => {
     });
 });
 
+router.post('/users', (req, res) => {
+    const params = {
+        TableName: table,
+        Item: {
+            'username': req.body.username,
+            'createdAt': Date.now(),
+            'thought': req.body.thought
+        }
+    };
+    dynamodb.put(params, (err, data) => {
+        if (err) {
+            console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+            res.status(500).json(err);
+        } else {
+            console.log('Added item:', JSON.stringify(data, null, 2));
+            res.json({'Added': JSON.stringify(data, null, 2)});
+        }
+    });
+});
+
 module.exports = router;
